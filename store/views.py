0970f0c003d4e3b6216a2d41c5aefd473e5c6216
Elena <whohasbudunec@gmail.com>
2024-12-21 21:35:05 +0300

    lab4 mandatory

--- store/views.py
+++ store/views.py
@@ -39,11 +39,17 @@ def products_view(request):
 
 
 
+# def shop_view(request):
+#     if request.method == "GET":
+#         with open('store/shop.html', encoding="utf-8") as f:
+#             data = f.read()  # Читаем HTML файл
+#         return HttpResponse(data)  # Отправляем HTML файл как ответ
+
+from django.shortcuts import render
+
 def shop_view(request):
     if request.method == "GET":
-        with open('store/shop.html', encoding="utf-8") as f:
-            data = f.read()  # Читаем HTML файл
-        return HttpResponse(data)  # Отправляем HTML файл как ответ
+        return render(request, 'store/shop.html', context={"products": DATABASE.values()})
 
 
 def products_page_view(request, page):
@@ -67,9 +73,18 @@ def products_page_view(request, page):
 
 def cart_view(request):
     if request.method == "GET":
-        data = view_in_cart() # TODO Вызвать ответственную за это действие функцию
-        return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
-                                                     'indent': 4})
+        data = view_in_cart()
+        if request.GET.get('format') == 'JSON':
+            return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
+                                                         'indent': 4})
+        products = []  # Список продуктов
+        for product_id, quantity in data['products'].items():
+            product = DATABASE[product_id]  # 1. Получите информацию о продукте из DATABASE по его product_id. product будет словарём
+            # 2. в словарь product под ключом "quantity" запишите текущее значение товара в корзине
+            product["price_total"] = f"{quantity * product['price_after']:.2f}"  # добавление общей цены позиции с ограничением в 2 знака
+            # 3. добавьте product в список products
+            products.append(product)
+        return render(request, "store/cart.html", context={"products": products})
 
 
 def cart_add_view(request, id_product):
